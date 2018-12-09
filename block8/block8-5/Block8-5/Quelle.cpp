304a113eb51f0ade2b1ce1da61b8183c9d3f3460
Lenoxy <lenoxy@gmx.ch>
2018-12-09 12:52:02 +0100

    homework

--- block8/block8-5/Block8-5/Quelle.cpp
+++ block8/block8-5/Block8-5/Quelle.cpp
@@ -5,7 +5,7 @@
 
 void main() {
 	char cWahl = '0';
-	float fRadiusAussen, fRadiusInnen, fHoehe, fVolumen = 0;
+	float fRadiusAussen = 0, fRadiusInnen = 0, fHoehe = 0, fVolumen = 0;
 	const float Pi = 3.1415926535;
 	printf("----------------------------------------\n");
 	printf("Berechnung von Geometrischen Körpern\n");
@@ -14,24 +14,35 @@ void main() {
 	scanf_s("%c", &cWahl);
 
 	if ('Z' == cWahl || 'z' == cWahl) {
-		printf("Radius:");
-		scanf_s("%f", &fHoehe);
-		printf("Hoehe:");
-		scanf_s("%f", &fHoehe);
-		fVolumen = Pi * fRadiusAussen * fRadiusAussen * fHoehe;
-		printf("Das Volumen ist: %0.3f", fVolumen);
+		zylinder(fHoehe, fVolumen, Pi, fRadiusAussen);
 	}
 	else if ('H' == cWahl ||'h' == cWahl){
-		printf("Radius Aussen:");
-		scanf_s("%f", &fRadiusAussen);
-		printf("Radius Innen:");
-		scanf_s("%f", &fRadiusInnen);
-		printf("\nHoehe");
-		scanf_s("%f", &fHoehe);
-		fVolumen = Pi * (fRadiusAussen * fRadiusAussen - fRadiusInnen * fRadiusInnen) * fHoehe;
-		printf("Das Volumen ist gleich: %f", &fVolumen);
-
-
+		hohlzylinder(fRadiusAussen, fRadiusInnen, fHoehe, Pi, fVolumen);
 	}
+
 	system("pause");
+}
+
+
+
+void hohlzylinder(float fRadiusAussen, float fRadiusInnen, float fHoehe, float Pi, float fVolumen) {
+
+	printf("Radius Aussen:");
+	scanf_s("%f", &fRadiusAussen);
+	printf("Radius Innen:");
+	scanf_s("%f", &fRadiusInnen);
+	printf("Hoehe:");
+	scanf_s("%f", &fHoehe);
+	fVolumen = Pi * (fRadiusAussen * fRadiusAussen - fRadiusInnen * fRadiusInnen) * fHoehe;
+	printf("Das Volumen ist gleich: %lg\n", &fVolumen);
+
+}
+
+void zylinder(float fHoehe, float fVolumen, float Pi, float fRadiusAussen) {
+	printf("Radius:");
+	scanf_s("%f", &fHoehe);
+	printf("Hoehe:");
+	scanf_s("%f", &fHoehe);
+	fVolumen = Pi * fRadiusAussen * fRadiusAussen * fHoehe;
+	printf("Das Volumen ist: %0.3f", fVolumen);
 }
